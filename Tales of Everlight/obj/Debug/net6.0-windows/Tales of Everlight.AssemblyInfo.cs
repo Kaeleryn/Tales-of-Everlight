01622a3cf500f57f029dff0c966f74f1d7a186ac
Kaeleryn <olexandrchschool@gmai.com>
2025-03-23 19:53:26 +0200

    Added files for double jump animation

--- Tales of Everlight/obj/Debug/net6.0-windows/Tales of Everlight.AssemblyInfo.cs
+++ Tales of Everlight/obj/Debug/net6.0-windows/Tales of Everlight.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e8c9efaf87ff9163b582930fe1c68c4b21ebbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11ddf09d20a8d5239859c466eb21e61bea24f0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
