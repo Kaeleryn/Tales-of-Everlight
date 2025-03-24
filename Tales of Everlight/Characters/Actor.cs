bc7e28ac79168ec48c09cb35c426b3d16fa9d0f2
Kaeleryn <olexandrchschool@gmai.com>
2025-03-25 01:35:33 +0200

    Animation of attack Possible fixes in future

--- Tales of Everlight/Characters/Actor.cs
+++ Tales of Everlight/Characters/Actor.cs
@@ -75,11 +75,17 @@ public abstract class Actor
     private int _jumpCount = 0; // Додаємо лічильник стрибків
     private const int MaxJumps = 2; // Два стрибки дозволені
 
+
+    public Rectangle sourceRectangle;
+    public Rectangle destinationRectangle;
+    public SpriteEffects spriteEffects;
+
     protected Actor(ContentManager content, Rectangle rect, Rectangle srect)
     {
         _currentFrame = 0;
         _runningTexture = content.Load<Texture2D>("animatedSprite");
         _jumpingTexture = content.Load<Texture2D>("animatedSpriteJumping");
+        _attackTexture = content.Load<Texture2D>("animatedSpriteAttacking");
 
         _rect = rect;
         _srect = srect;
@@ -88,10 +94,17 @@ public abstract class Actor
 
     protected Actor() => _velocity = Vector2.Zero;
 
-    public void HandleMovement(KeyboardState keystate, KeyboardState previousState, GameTime gameTime)
+    public void HandleMovement(KeyboardState keystate, KeyboardState previousState, MouseState mouseState,
+        MouseState previousMState, GameTime gameTime)
     {
-        HandleVerticalMovement(keystate, previousState, gameTime);
-        HandleHorisontalMovement(keystate, previousState, gameTime);
+        if (AnimationState != AnimationState.Attacking)
+        {
+            HandleVerticalMovement(keystate, previousState, gameTime);
+            HandleHorisontalMovement(keystate, previousState, gameTime);
+        }
+        // HandleAttack(mouseState, previousMState, gameTime);
+
+        HandleAttackMovement(mouseState, previousMState, gameTime);
     }
 
     public void HandleVerticalMovement(KeyboardState keystate, KeyboardState previousState, GameTime gameTime)
@@ -113,44 +126,44 @@ public abstract class Actor
         }
     }
 
-    public void HamdleAttack(MouseState mouseState, MouseState previousState, GameTime gameTime)
-    {
-        if(mouseState.LeftButton == ButtonState.Pressed && previousState.LeftButton == ButtonState.Released)
-        {
-            AnimationState = AnimationState.Attacking;
+    // public void HandleAttack(MouseState mouseState, MouseState previousState, GameTime gameTime)
+    // {
+    //     if (mouseState.LeftButton == ButtonState.Pressed && previousState.LeftButton == ButtonState.Released)
+    //     {
+    //         AnimationState = AnimationState.Attacking;
+    //
+    //         _currentFrame = 0;
+    //     }
+    //
+    //     if (AnimationState == AnimationState.Attacking)
+    //     {
+    //         UpdateAttackFrame(gameTime);
+    //     }
+    // }
+    //
+    // private void UpdateAttackFrame(GameTime gameTime)
+    // {
+    //     _timeSinceLastFrame += gameTime.ElapsedGameTime.TotalSeconds;
+    //
+    //     if (_timeSinceLastFrame >= FrameTime)
+    //     {
+    //         _timeSinceLastFrame -= FrameTime;
+    //
+    //         // Move to next frame in attack animation
+    //         if (_currentFrame < _totalFrames - 1)
+    //         {
+    //             _currentFrame++;
+    //         }
+    //         else
+    //         {
+    //             // Attack animation completed
+    //             _currentFrame = 0;
+    //             // Return to previous state when attack is done
+    //             AnimationState = IsOnGround ? AnimationState.Running : AnimationState.Jumping;
+    //         }
+    //     }
+    // }
 
-            _currentFrame = 0;
-        }
-        
-        if (AnimationState == AnimationState.Attacking)
-        {
-            UpdateAttackFrame(gameTime);
-        }
-    }
-    
-    private void UpdateAttackFrame(GameTime gameTime)
-    {
-        _timeSinceLastFrame += gameTime.ElapsedGameTime.TotalSeconds;
-
-        if (_timeSinceLastFrame >= FrameTime)
-        {
-            _timeSinceLastFrame -= FrameTime;
-        
-            // Move to next frame in attack animation
-            if (_currentFrame < _totalFrames - 1)
-            {
-                _currentFrame++;
-            }
-            else
-            {
-                // Attack animation completed
-                _currentFrame = 0;
-                // Return to previous state when attack is done
-                AnimationState = IsOnGround ? AnimationState.Running : AnimationState.Jumping;
-            }
-        }
-    } 
-    
 
     public void HandleHorisontalMovement(KeyboardState keystate, KeyboardState previousState, GameTime gameTime)
     {
@@ -178,6 +191,22 @@ public abstract class Actor
         }
     }
 
+
+    public void HandleAttackMovement(MouseState mousestate, MouseState previousMState, GameTime gameTime)
+    {
+        if (mousestate.LeftButton == ButtonState.Pressed)
+        {
+            _velocity = Vector2.Zero; // Зупиняємо персонажа
+            AnimationState = AnimationState.Attacking; // Змінюємо стан анімації
+            _currentFrame = 0; // Скидаємо кадр анімації
+        }
+
+        if (AnimationState == AnimationState.Attacking)
+        {
+            UpdateFrame(gameTime);
+        }
+    }
+
     public void Decelerate()
     {
         _velocity.X *= 0.8f;
@@ -185,6 +214,34 @@ public abstract class Actor
         if (_velocity.X < 0.5f && _velocity.X > -0.5f)
             _velocity.X = 0;
     }
+
+    // private void UpdateFrame(GameTime gameTime)
+    // {
+    //     _timeSinceLastFrame += gameTime.ElapsedGameTime.TotalSeconds;
+    //
+    //     if (_timeSinceLastFrame >= FrameTime)
+    //     {
+    //         _timeSinceLastFrame -= FrameTime;
+    //
+    //         if (AnimationState == AnimationState.Jumping && !IsOnGround)
+    //         {
+    //             if (_currentFrame < _totalFrames - 1)
+    //             {
+    //                 _currentFrame = (_currentFrame + 1) % _totalFrames;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             _currentFrame = (_currentFrame + 1) % _totalFrames;
+    //
+    //             if (AnimationState == AnimationState.Running && (_currentFrame == 0 || _currentFrame == 3))
+    //             {
+    //                 _stepsDone++;
+    //             }
+    //         }
+    //     }
+    // }
+
 
     private void UpdateFrame(GameTime gameTime)
     {
@@ -194,7 +251,22 @@ public abstract class Actor
         {
             _timeSinceLastFrame -= FrameTime;
 
-            if (AnimationState == AnimationState.Jumping && !IsOnGround)
+            if (AnimationState == AnimationState.Attacking)
+            {
+                // Move to next frame in attack animation
+                if (_currentFrame < _totalFrames - 1)
+                {
+                    _currentFrame = (_currentFrame + 1) % _totalFrames;
+                }
+                else
+                {
+                    // Attack animation completed
+                    _currentFrame = 0;
+                    // Return to previous state when attack is done
+                    AnimationState = IsOnGround ? AnimationState.Running : AnimationState.Jumping;
+                }
+            }
+            else if (AnimationState == AnimationState.Jumping && !IsOnGround)
             {
                 if (_currentFrame < _totalFrames - 1)
                 {
@@ -213,26 +285,24 @@ public abstract class Actor
         }
     }
 
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        spriteBatch.Draw(_runningTexture, _rect, _srect, Color.White);
-    }
-
-    public void Draw(SpriteBatch spriteBatch, Vector2 location)
+    private void AnimationHandler()
     {
         switch (AnimationState)
         {
             case AnimationState.Running:
                 _currentTexture = _runningTexture;
                 Columns = 6;
+                Rows = 1;
                 break;
             case AnimationState.Jumping:
                 _currentTexture = _jumpingTexture;
                 Columns = 7;
+                Rows = 1;
                 break;
             case AnimationState.Attacking:
                 _currentTexture = _attackTexture;
-                Columns = 5;
+                Columns = 1;
+                Rows = 8;
                 break;
         }
 
@@ -242,6 +312,16 @@ public abstract class Actor
             _currentTexture = _runningTexture;
             Columns = 6;
         }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_runningTexture, _rect, _srect, Color.White);
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Vector2 location)
+    {
+        AnimationHandler();
 
         _totalFrames = Columns * Rows;
         int width = _currentTexture.Width / Columns;
@@ -249,11 +329,31 @@ public abstract class Actor
         int row = _currentFrame / Columns;
         int column = _currentFrame % Columns;
 
-        Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
-        SpriteEffects spriteEffects = _isFacingRight ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
-        Rectangle destinationRectangle = new Rectangle((int)location.X - 80, (int)location.Y, width, height);
+        if (AnimationState != AnimationState.Attacking)
+        {
+            sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            destinationRectangle = new Rectangle((int)location.X - 80, (int)location.Y, width, height);
+            spriteEffects = _isFacingRight ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+        }
+        else // Анімація атаки
+        {
+            if (_isFacingRight)
+            {
+                sourceRectangle = new Rectangle(width * column, height * row, width, height);
+                destinationRectangle = new Rectangle((int)location.X - 20, (int)location.Y - 41, width, height);
+                spriteEffects = _isFacingRight ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            }
+            else // Чесно, не моє поняття як це зробити без хардкода
+            {
+                sourceRectangle = new Rectangle(width * column, height * row, width, height);
+                destinationRectangle = new Rectangle((int)location.X - 80, (int)location.Y - 41, width, height);
+                spriteEffects = _isFacingRight ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            }
+        }
 
-        spriteBatch.Draw(_currentTexture, destinationRectangle, sourceRectangle, Color.White, 0f, Vector2.Zero, spriteEffects, 0f);
+
+        spriteBatch.Draw(_currentTexture, destinationRectangle, sourceRectangle, Color.White, 0f, Vector2.Zero,
+            spriteEffects, 0f);
     }
 
     public void DrawBoundingBox(SpriteBatch spriteBatch, Texture2D hitboxTexture)
