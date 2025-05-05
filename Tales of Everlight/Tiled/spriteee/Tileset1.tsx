3998b216e8b4b77c2eb5f5a95e2e76778fa3181e
PC <dimagusak5@gmail.com>
2025-05-05 17:12:51 +0300

    Map ver.2.0

---
+++ Tales of Everlight/Tiled/spriteee/Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset1" tilewidth="64" tileheight="64" tilecount="180" columns="15">
+ <image source="Tileset.png" width="960" height="768"/>
+</tileset>
