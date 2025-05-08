2adfd369deede7eb676815477b5464acfe0b830a
PC <dimagusak5@gmail.com>
2025-05-08 19:02:13 +0300

    Level2 ver.0

---
+++ Tales of Everlight/Tiled/cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="cave" tilewidth="64" tileheight="64" tilecount="784" columns="28">
+ <image source="C:/Users/PC/Downloads/Cave Tile set.png" width="1800" height="1800"/>
+</tileset>
