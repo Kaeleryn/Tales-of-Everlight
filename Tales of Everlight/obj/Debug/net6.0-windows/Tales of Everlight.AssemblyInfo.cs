bedfee7baa65b9021b87918f0d47601d63ed9df6
makvas <andriymvnaruto@gmail.com>
2025-06-04 19:06:06 +0300

    fixed menus

--- Tales of Everlight/obj/Debug/net6.0-windows/Tales of Everlight.AssemblyInfo.cs
+++ Tales of Everlight/obj/Debug/net6.0-windows/Tales of Everlight.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4d0e9e0a9854cad087e503a4370097b3fdebf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tales of Everlight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
